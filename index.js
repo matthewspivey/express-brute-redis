6a13a229b827c0331410bf36b90fc25435db2ae8
Matt Spivey <spivey.matthew@gmail.com>
2019-03-03 18:53:24 -0500

    Strip broken incremnt code

--- index.js
+++ index.js
@@ -1,45 +1,59 @@
+function toRedisValue(count = 0, lastRequest = null, firstRequest = null) {
+  return JSON.stringify({
+    count,
+    lastRequest,
+    firstRequest
+  });
+}
+
 class RedisStore {
   constructor({ prefix, client }) {
     this.prefix = prefix || '';
     this.client = client;
   }
 
+  redisKey(key) {
+    return this.prefix + key;
+  }
+
   set(key, value, lifetime, callback) {
     const lifetimeInt = parseInt(lifetime, 10) || 0;
-    const multi = this.client.multi();
-    const redisKey = this.prefix + key;
-
-    multi.set(redisKey, JSON.stringify(value));
-    if (lifetimeInt > 0) {
-      multi.expire(redisKey, lifetimeInt);
-    }
-    multi.exec(() => {
+    const doCallback = () => {
       if (typeof callback === 'function') {
         callback.call(this, null);
       }
-    });
+    };
+    const redisValue = toRedisValue(value.count, value.lastRequest, value.firstRequest);
+
+    if (lifetimeInt > 0) {
+      this.client.set(this.redisKey(key), redisValue, 'EX', lifetimeInt, doCallback);
+    } else {
+      this.client.set(this.redisKey(key), redisValue, doCallback);
+    }
   }
 
   get(key, callback) {
-    this.client.get(this.prefix + key, (err, data) => {
-      if (typeof callback !== 'function') {
-        return;
-      }
+    if (typeof callback !== 'function') {
+      return;
+    }
+
+    this.client.get(this.redisKey(key), (err, data) => {
       if (err || !data) {
         callback(err, null);
-      } else {
-        const parsed = JSON.parse(data);
-        callback(err, {
-          ...parsed,
-          lastRequest: new Date(parsed.lastRequest),
-          firstRequest: new Date(parsed.firstRequest),
-        });
+        return;
       }
+
+      const { count, lastRequest, firstRequest } = JSON.parse(data);
+      callback(err, {
+        count,
+        lastRequest: new Date(lastRequest),
+        firstRequest: new Date(firstRequest)
+      });
     });
   }
 
   reset(key, callback) {
-    this.client.del(this.prefix + key, (...args) => {
+    this.client.del(this.redisKey(key), (...args) => {
       if (typeof callback === 'function') {
         callback.apply(this, args);
       }
@@ -48,29 +62,6 @@ class RedisStore {
 
   // TODO can we make this a more atomic operation using Redis eval
   increment(key, lifetime, callback) {
-    const self = this;
-    this.get(key, (err, value) => {
-      if (err) {
-        callback(err);
-        return;
-      }
-      const count = value ? value.count + 1 : 1;
-      self.set(
-        key,
-        { count, lastRequest: new Date(), firstRequest: new Date() },
-        lifetime,
-        (err2) => {
-          const prevValue = {
-            count: value ? value.count : 0,
-            lastRequest: value ? value.lastRequest : null,
-            firstRequest: value ? value.firstRequest : null,
-          };
-          if (typeof callback === 'function') {
-            callback(err2, prevValue);
-          }
-        },
-      );
-    });
   }
 }
 module.exports = RedisStore;
